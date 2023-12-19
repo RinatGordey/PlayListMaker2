5505d3aa0cb63ae4ecdcf1a743187f2ab8d988d1
Rinat <o-roman@list.ru>
2023-12-19 18:58:27 +0500

    Домашнее задание Спринт 11

--- app/src/main/java/com/example/playlistmaker2/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker2/SearchActivity.kt
@@ -1,97 +1,252 @@
 package com.example.playlistmaker2
+
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
-import android.content.Context
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.playlistmake2.ItunesService
 import com.example.playlistmaker2.databinding.ActivitySearchBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 
 class SearchActivity : AppCompatActivity() {
-    companion object{
+    private lateinit var binding: ActivitySearchBinding
+
+    companion object {
         const val SEARCH_ACTIVITY = "SearchActivity"
         const val SEARCH_TEXT = "searchText"
     }
 
-    private lateinit var searchEditText: EditText
-    private lateinit var trackRecyclerView: RecyclerView
-    private lateinit var trackAdapter: TrackAdapter
+    private val urlApi = "https://itunes.apple.com"
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(urlApi)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val iTunseService = retrofit.create(ItunesService::class.java)
+
+    private val trackList = mutableListOf<Track>()
+
+
+    private lateinit var edSearch: EditText
+    private lateinit var placeholderView: LinearLayout
+    private lateinit var adapter: TrackAdapter
+    private lateinit var btClear: ImageButton
+    private lateinit var rvTrack: RecyclerView
+    private lateinit var btBackSearch: ImageView
+    private lateinit var ivPlaceholder: ImageView
+    private lateinit var tvPlaceholder: TextView
+    private lateinit var btRefresh: Button
+    private var lastSearchQuery: String? = null
+
     private var searchText: String = ""
-    private val listTracks = mutableListOf<Track>()
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivitySearchBinding.inflate(layoutInflater)
+        binding = ActivitySearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val backButton = findViewById<ImageButton>(R.id.btBackSearch)
-        backButton.setOnClickListener {
+        edSearch = binding.edSearch
+        btClear = binding.btClear
+        placeholderView = binding.placeholderView
+        rvTrack = binding.rvTrack
+        btBackSearch = binding.btBackSearch
+        ivPlaceholder = binding.ivPlaceholder
+        tvPlaceholder = binding.tvPlaceholder
+        btRefresh = binding.btRefresh
+        startRvTrack()
+
+
+        btBackSearch.setOnClickListener {
             val returnIntent = Intent()
             returnIntent.putExtra(SEARCH_TEXT, searchText)
             setResult(Activity.RESULT_OK, returnIntent)
             finish() // Закрываем эту активити при нажатии "назад"
         }
-        trackAdapter = TrackAdapter()
-
-        recyclerViewTrack()
-
-        listTracks.addAll(arrayTrackList)
-
-        searchEditText = findViewById(R.id.edSearch)
-
-        val clearButton = findViewById<ImageButton>(R.id.btClear)
 
         // Восстановить значение из SharedPreferences, если оно было сохранено
         val sharedPreferences = getSharedPreferences(SEARCH_ACTIVITY, Context.MODE_PRIVATE)
         searchText = sharedPreferences.getString(SEARCH_TEXT, "") ?: ""
-        searchEditText.setText(searchText)
+        edSearch.setText(searchText)
 
         if (savedInstanceState != null) {
             searchText = savedInstanceState.getString(SEARCH_TEXT, "")
-            searchEditText.setText(searchText)
+            edSearch.setText(searchText)
         }
 
-        searchEditText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                clearButton.visibility = if (s.isNotEmpty()) View.VISIBLE else View.GONE
-                searchText = s.toString()
-
-            }
-            override fun afterTextChanged(s: Editable) {}
-        })
-
-        clearButton.setOnClickListener {
-            searchEditText.text.clear()
+        btClear.setOnClickListener {
+            edSearch.setText("")
 
             // Скрыть клавиатуру
             val hideKeyboard =
                 getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            hideKeyboard.hideSoftInputFromWindow(searchEditText.windowToken, 0)
+            hideKeyboard.hideSoftInputFromWindow(edSearch.windowToken, 0)
 
             // Очистить значение из SharedPreferences
             val editor = sharedPreferences.edit()
             editor.putString(SEARCH_TEXT, "")
             editor.apply()
+            btClear.visibility = View.GONE
+            rvTrack.visibility = View.GONE
+            tvPlaceholder.visibility = View.GONE
+            ivPlaceholder.visibility = View.GONE
+            btRefresh.visibility = View.GONE
+        }
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                btClear.visibility = if (s.isNotEmpty()) View.VISIBLE else View.GONE
+                searchText = s.toString()
+
+            }
+            override fun afterTextChanged(s: Editable) {}
+        }
+        edSearch.addTextChangedListener(textWatcher)
+
+        edSearch.setOnClickListener {
+            edSearch.text.clear()
+
+        }
+        btRefresh.setOnClickListener {
+            val query = lastSearchQuery
+            if (query != null) {
+                startSearch(query)
+            }
+        }
+        edSearch.setOnEditorActionListener { _, actionId, _ ->
+            val input = edSearch.text.toString()
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                startSearch(input)
+                lastSearchQuery = input
+
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(edSearch.windowToken, 0)
+                return@setOnEditorActionListener true
+            }
+            return@setOnEditorActionListener false
+        }
+        // Обработка нажатия на кнопку "Назад"
+        btBackSearch.setOnClickListener {
+            finish()
         }
     }
+
+    private fun startSearch(query: String) {
+        // отправляем запрос в itunes
+        iTunseService.search(query).enqueue(object : Callback<TrackResponse> {
+            @SuppressLint("NotifyDataSetChanged")
+            override fun onResponse(call: Call<TrackResponse>, response: Response<TrackResponse>) {
+                val bodyResponse = response.body()?.results
+                if (response.isSuccessful) {
+                    trackList.clear()
+                    if (bodyResponse?.isNotEmpty() == true) {
+                        trackList.addAll(bodyResponse)
+                        adapter.notifyDataSetChanged()
+                        rvTrack.visibility = View.VISIBLE
+                    }
+                    val nightModeFlags = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+                    if (trackList.isEmpty()) {
+                        ivPlaceholder.visibility = View.VISIBLE
+                        ivPlaceholder.setImageResource(
+                            if (nightModeFlags == Configuration.UI_MODE_NIGHT_YES) {
+                                R.drawable.ic_error_faund_track_night
+                            } else {
+                                R.drawable.ic_error_faund_track
+                            }
+                        )
+                        showMessage(getString(R.string.error_tracks), "")
+                    } else {
+                        showMessage("", "")
+                    }
+                    btRefresh.visibility = View.GONE
+                } else {
+                    lastSearchQuery = query
+                    ivPlaceholder.visibility = View.VISIBLE
+                    showMessage(
+                        getString(R.string.error_to_link),
+                        response.code().toString()
+                    )
+                }
+            }
+
+            override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                lastSearchQuery = query
+                ivPlaceholder.visibility = View.VISIBLE
+                ivPlaceholder.setImageResource(
+                    if (isNightModeEnabled()) {
+                        R.drawable.ic_error_to_link_night
+                    } else {
+                        R.drawable.ic_error_to_link
+                    }
+                )
+                showMessage(getString(R.string.error_to_link), t.message.toString())
+                btRefresh.visibility = View.VISIBLE
+            }
+
+            private fun isNightModeEnabled(): Boolean {
+                val nightModeFlags = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+                return nightModeFlags == Configuration.UI_MODE_NIGHT_YES
+            }
+        })
+    }
+
+    private fun showMessage(text: String, additionalMessage: String) {
+
+        if (text.isNotEmpty()) {
+            trackList.clear()
+            adapter.notifyDataSetChanged()
+            tvPlaceholder.visibility = View.VISIBLE
+            tvPlaceholder.text = text
+            if (additionalMessage.isNotEmpty()) {
+                Toast.makeText(applicationContext, additionalMessage, Toast.LENGTH_LONG)
+                    .show()
+            }
+        } else {
+            tvPlaceholder.visibility = View.GONE
+        }
+    }
+
+    private fun startRvTrack() {
+        adapter = TrackAdapter(trackList)
+        rvTrack.layoutManager = LinearLayoutManager(this)
+        rvTrack.adapter = adapter
+    }
+
     override fun onSaveInstanceState(outState: Bundle) {
-        outState.putString(SEARCH_TEXT, searchText)
         super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_TEXT, edSearch.text.toString())
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         searchText = savedInstanceState.getString(SEARCH_TEXT, "")
-        searchEditText.setText(searchText)
+        edSearch.setText(searchText)
     }
 
     override fun onPause() {
@@ -102,15 +257,5 @@ class SearchActivity : AppCompatActivity() {
         val editor = sharedPreferences.edit()
         editor.putString(SEARCH_TEXT, searchText)
         editor.apply()
-    }
-    private fun recyclerViewTrack() {
-        trackRecyclerView = findViewById(R.id.rvTrack)
-        val layoutManager = LinearLayoutManager(this)
-        trackRecyclerView.layoutManager = layoutManager
-
-        trackAdapter.setTrackList(arrayTrackList)
-
-        trackRecyclerView.adapter = trackAdapter
-
     }
 }
